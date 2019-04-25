967fed6348d41e914887a1be482c90ed677caba9
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-04-25 19:18:59 +0100

    updating

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Title from './Title';
 import { Element } from 'react-scroll';
 
-import { BLUE, RED, BOX_SHADOW2 } from '../styles/styles';
+import { RED, BOX_SHADOW2 } from '../styles/styles';
 
 
 
@@ -12,51 +12,65 @@ const projects = [
     {
         img: 'https://images.pexels.com/photos/209151/pexels-photo-209151.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         title: 'Website',
-        description: 'awesome website'
+        description: 'awesome website',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     },
     {
         img: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         title: 'Website2',
-        description: 'awesome website2'
+        description: 'awesome website2',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     },
     {
-        img: 'https://images.pexels.com/photos/209151/pexels-photo-209151.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        title: 'Website',
-        description: 'awesome website'
+        img: 'https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: 'Website3',
+        description: 'awesome website3',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     },
     {
-        img: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        title: 'Website2',
-        description: 'awesome website2'
+        img: 'https://images.pexels.com/photos/1266007/pexels-photo-1266007.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: 'Website4',
+        description: 'awesome website4',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     },
     {
-        img: 'https://images.pexels.com/photos/209151/pexels-photo-209151.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        title: 'Website',
-        description: 'awesome website'
+        img: 'https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: 'Website5',
+        description: 'awesome website5',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     },
     {
-        img: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        title: 'Website2',
-        description: 'awesome website2'
+        img: 'https://images.pexels.com/photos/106344/pexels-photo-106344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: 'Website6',
+        description: 'awesome website6',
+        github: 'https://github.com/MarioSilvaPrada/color_game'
     }
 ];
 
 const StyledPortfolio = styled.div`
     display: flex;
     flex-direction: column;
+    
 
     .my-work {
         display: flex;
         flex-wrap:wrap;
+        ${'' /* width: 90%;
+        margin: 0 auto; */}
+        justify-content: space-evenly;
     }
 
     .project {
-       width: 400px;
+       width: 350px; 
+       height: 400px;
        margin: 20px;
        border: 1px solid #d0d0d0;
         ${BOX_SHADOW2}
+    }
 
-
+    .project a {
+        text-decoration: none;
+        color: black;
     }
 
     .project img {
@@ -64,8 +78,26 @@ const StyledPortfolio = styled.div`
         height: 60%;
     }
 
-    a {
-        text-decoration: none;
+    .project h1 {
+        font-size: 25px;
+    }
+
+    .project h1:hover {
+        text-decoration:underline;
+    }
+
+    .project i {
+        font-size: 1.2rem;
+        margin-left: 8px;
+    }
+
+    .project .github {
+        color: #999999;
+        transition: 0.5s;
+    }
+
+    .project .github:hover {
+        color: ${RED}
     }
 `
 
@@ -79,11 +111,18 @@ const Portfolio = () => (
             <div className='my-work'>
                 {projects.map(project => {
                     return <div className="project">
-                        <a href={project.img}>
+                        <a href={project.img} target="_blank">
                             <img alt={project.title} src={project.img} />
-                            <h1>{project.title}</h1>
-                            <p>{project.description}</p>
                         </a>
+                        <a href={project.img} target="_blank">
+                            <h1>{project.title}</h1>
+                        </a>
+                        <p>{project.description}</p>
+                        <a href={project.github} className="github" target="_blank">
+                            <span>GitHub</span>
+                            <i class="fab fa-github"></i>
+                        </a>
+
                     </div>
                 })}
             </div>
