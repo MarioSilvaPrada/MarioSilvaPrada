2d7f9681f1f959a4e9d75aa77be48e979d10de91
MarioPradaGenerous <mario@generous.studio>
2019-07-28 10:06:24 +0100

    adding button animation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Header from './components/Header/Header';
-import Greetings from './components/Greetings/Greetings';
-import AboutMe from './components/AboutMe/AboutMe';
-import Portfolio from './components/Portfolio/Portfolio';
-import Development from './components/Development/Development';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import FloatingButton from './styles/button/FloatingButton';
-
-
+import Header from "./components/Header/Header";
+import Greetings from "./components/Greetings/Greetings";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Development from "./components/Development/Development";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import FloatingButton from "./styles/button/FloatingButton";
 
 class App extends Component {
-
   state = {
     position: 0
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.listenToScroll);
+    window.addEventListener("scroll", this.listenToScroll);
   }
 
   listenToScroll = () => {
-    const winScroll = window.pageYOffset
-  
+    const winScroll = window.pageYOffset;
+
     this.setState({
-      position: winScroll,
-    })
-  }
+      position: winScroll
+    });
+  };
 
   render() {
-
     const { position } = this.state;
 
-    let floatbtn = position > 50 ? <FloatingButton /> : '';
+    let floatbtn = position > 50 ? <FloatingButton /> : "";
 
     return (
       <div className="App">
--- src/styles/button/FloatingButton.jsx
+++ src/styles/button/FloatingButton.jsx
@@ -1,55 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-scroll';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { Link } from "react-scroll";
+import { zoomIn } from "react-animations";
 
-import { DARK_RED, BOX_SHADOW } from '../../styles/styles';
+import { DARK_RED, BOX_SHADOW } from "../../styles/styles";
 
+const animation = keyframes`${zoomIn}`;
 
 const FloatingButtonStyled = styled.button`
-    height: 4rem;
-    width: 4rem;
-    border-radius: 100%;
-    border: 1px solid ${DARK_RED};
-    background: ${DARK_RED};
-    position: fixed;
-    ${BOX_SHADOW}
+  animation: 2s ${animation};
+  height: 4rem;
+  width: 4rem;
+  border-radius: 100%;
+  border: 1px solid ${DARK_RED};
+  background: ${DARK_RED};
+  position: fixed;
+  ${BOX_SHADOW}
+  top: 85%;
+  left: 92%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 2.5rem;
+  cursor: pointer;
+
+  @media screen and (max-width: 950px) {
     top: 85%;
-    left: 92%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 2.5rem;
+    left: 88%;
+  }
 
-    :hover {
-        cursor: pointer;
-    }
-
-    @media screen and (max-width: 950px) {
-        top: 85%;
-        left: 88%;
-    }
-
-    @media screen and (max-width: 500px) {
-        top: 90%;
-        left: 85%;
-    }
-
-
-`
+  @media screen and (max-width: 500px) {
+    top: 90%;
+    left: 85%;
+  }
+`;
 
 const FloatingButton = () => (
+  <FloatingButtonStyled className="top-btn">
+    <Link
+      activeClass="active"
+      to="navbar"
+      spy={true}
+      smooth={true}
+      duration={700}
+    >
+      <i className="fas fa-chevron-up" />
+    </Link>
+  </FloatingButtonStyled>
+);
 
-    <FloatingButtonStyled className="top-btn">
-
-        <Link activeClass="active" to="navbar" spy={true} smooth={true} duration={700}>
-            <i className="fas fa-chevron-up"></i>
-        </Link>
-
-    </FloatingButtonStyled>
-
-
-
-)
-
-export default FloatingButton
+export default FloatingButton;
